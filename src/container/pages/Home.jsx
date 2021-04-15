16e3637204160154bcc448f1655570ce195449b8
Aziz Faozi <admin@labseni.com>
2021-04-15 16:34:23 +0700

    mantap

--- src/container/pages/Home.jsx
+++ src/container/pages/Home.jsx
@@ -55,31 +55,6 @@ const Home = () => {
 		  test frament 
 		 */
 	    }
-            <div>
-	      <Fragment>
-		<input
-		  type="text"
-		  value={query}
-		  onChange={event=>setQuery(event.target.value)}
-		  />
-		  <button type="button" onClick={()=>setSearch(query)}>
-		    Search
-		  </button>
-
-		  <ul>
-		    {
-			data.hits.map(item=>(
-			    <li key={item.objectID}>
-			      <a href={item.url}>{item.title}</a>
-			    </li>
-			
-
-			)
-		    )}
-		  </ul>
-		</Fragment>
-	    </div>
-
 	    
 
             <div>
